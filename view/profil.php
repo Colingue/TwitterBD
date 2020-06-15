3b4d877ad80820f4c6f2957e0fe34106de9efb5c
Lucas PAULO <lucas.paulo@etu.uca.fr>
2020-06-15 17:12:33 +0200

    Ajout editer + deconnecter

--- view/profil.php
+++ view/profil.php
@@ -23,14 +23,29 @@ if (isset($_GET['id']) AND $_GET['id'] > 0)
         <div align="center">
             <h2>Votre profil <?php echo $user['pseudo']; ?></h2>
             <br/> <br/>
-            
+            Pseudo : <?php echo $user['pseudo']; ?>
+            <br/>
+            Mail : <?php echo $user['mail']; ?>
             <br/>
             <?php
-            if(isset($message)) 
+            if(isset($_SESSION['id']) AND $user['id'] == $_SESSION['id'])
             {
-                echo $message;
+            ?>
+            <a href="#">Editer mon profil</a>
+                <a href="deconnection.php">Se déconnecter</a>
+            <?php
             }
             ?>
+        </div>
+            <div>
+                <h2>Vous suive</h2>
+                <ul>
+                    <li></li>
+                </ul>
+            </div>
+            <div>
+                <h2>Abonnements</h2>
+            </div>
         </div>
     </body>
 </html>
