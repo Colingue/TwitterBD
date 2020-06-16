07898f5ec0e93bbe7d42af5fb820571cfe7e24c0
Colin NOIRET <Colin.NOIRET@etu.uca.fr>
2020-06-16 19:00:17 +0200

    Ajout des tweets dans la base de données avec l'ecriture en timeline

--- view/timeline.php
+++ view/timeline.php
@@ -3,6 +3,26 @@ session_start();
 
 $bdd = new PDO('mysql:host=127.0.0.1;dbname=user__data', 'root', '');
 
+if(isset($_POST['tweet_form']))
+{
+    if(isset($_POST['tweet']) AND !empty($_POST['tweet']))
+    {
+        $tweet = htmlspecialchars($_POST['tweet']);
+
+        if(strlen($tweet) <= 140)
+        {
+            //Prends la date Now
+            $queryDate = $bdd->prepare('SELECT NOW()');
+            $queryDate->execute();
+            $date = $queryDate->fetch();
+            
+            // Insert un nouveau tweet dans le base de données
+            $query = $bdd->prepare('INSERT INTO tweet (tweet_user_id, tweet_like , tweet_date, tweet_message) VALUES (?, ?, now(), ?)');
+            $query->execute(array($_GET['id'], 0, $tweet));
+        }
+    }
+}
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -22,7 +42,7 @@ $bdd = new PDO('mysql:host=127.0.0.1;dbname=user__data', 'root', '');
                 <h2>Home</h2>
                 <form method="POST" action="">
                     <textarea width=500 name="tweet" placeholder="Avez vous quelque chose à dire ?"></textarea></br></br>
-                    <input type="submit" value="Tweeter !" />
+                    <input type="submit" value="Tweeter !" name="tweet_form" />
                 </form>
                 <?php if(isset($message)) { echo $message; } ?>
             </div>
