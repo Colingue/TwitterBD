7d403bee106673ff6870ac3b47730828c6b6e0af
Colin NOIRET <Colin.NOIRET@etu.uca.fr>
2020-06-17 18:19:24 +0200

    plis

--- view/timeline.php
+++ view/timeline.php
@@ -29,12 +29,8 @@ if(isset($_POST['tweet_form']))
 $tweet = $bdd->prepare('SELECT t.tweet_id, t.tweet_user_id, t.tweet_like, t.tweet_date, t.tweet_message FROM tweet AS t
                         INNER JOIN subscriptions AS s ON t.tweet_user_id = s.subscriptions_follow_ups_id WHERE s.subscriptions_follower_id = ?
                         UNION
-<<<<<<< HEAD
-                        SELECT t.tweet_id, t.tweet_user_id, t.tweet_like, t.tweet_date, t.tweet_message FROM tweet AS t WHERE t.tweet_user_id = ?');
-=======
                         SELECT t.tweet_id, t.tweet_user_id, t.tweet_like, t.tweet_date, t.tweet_message FROM tweet AS t WHERE t.tweet_user_id = ?
                         ORDER BY tweet_date DESC');
->>>>>>> ba4fba3eb62704ff5f356ad958313f16587efe5c
 $tweet->execute(array($_GET['id'], $_GET['id']));
 $tweet_tl = $tweet->fetchall();
 
@@ -67,17 +63,11 @@ $tweet_tl = $tweet->fetchall();
                     </br><input type="submit" value="Tweeter !" name="tweet_form" /></br> 
                 </form>
                 <?php if(isset($message)) { echo $message; } ?>
-<<<<<<< HEAD
-                <div class="affichage-tweets">
-                    </br><?php for ($lign = 0; $lign < count($tweet_tl); $lign++) {
-                    $tweet_info = $bdd->prepare('SELECT u.pseudo FROM user AS u INNER JOIN tweet AS t ON u.id = t.tweet_user_id WHERE t.tweet_user_id = ? GROUP BY u.pseudo');
-=======
 
                 <div>
                     </br>
                     <?php for ($lign = 0; $lign < count($tweet_tl); $lign++) {
                     $tweet_info = $bdd->prepare('SELECT u.pseudo, u.id FROM user AS u INNER JOIN tweet AS t ON u.id = t.tweet_user_id WHERE t.tweet_user_id = ? GROUP BY u.pseudo');
->>>>>>> ba4fba3eb62704ff5f356ad958313f16587efe5c
                     $tweet_info->execute(array($tweet_tl[$lign]['tweet_user_id']));
                     $pseudo = $tweet_info->fetch(); ?>
                         <b> <a href="public_profil.php?id=<?php echo $_SESSION['id'];?>&public_id=<?php echo $pseudo['id']; ?>"><?= $pseudo['pseudo']?>:</a></b><?= $tweet_tl[$lign]['tweet_message'] ?></br>
